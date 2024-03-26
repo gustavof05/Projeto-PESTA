9af2ddc241157dbf7fca201ec15fd54b486c932b
Gustavo Fialho <gustavosalgueirofialho@gmail.com>
2024-03-26 12:06:59 +0000

    1º Commit

---
+++ sub_rel.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Portal do DEE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&copy;DEE.Net 2012-2024</title>
+ </head>
+ <body>
+  <div style=text-align:center;font-size:15.0pt;><b>Portal do DEE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&copy;DEE.Net 2012-2024</b></div></p>
+  <div style=text-align:center;font-size:30.0pt;>
+   <?php 
+    if($_POST["FiltroDisciplinas"]=="1") echo "LABSI";
+    else echo "PESTA";
+   ?>
+ </body>
+</html>
