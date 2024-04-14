8c017885728fc9a4c434534abf4661402ef3960c
Gustavo Fialho <gustavosalgueirofialho@gmail.com>
2024-04-14 02:26:18 +0100

    7º Commit
    JS a funcionar e BD já identifica se há submissões abertas ou fechadas

--- sub_rel.php
+++ sub_rel.php
@@ -1,53 +1,27 @@
-<!--?php
-  $conexao = new SQLite3('caminho_para_seu_banco_de_dados.sqlite');
-  if (!$conexao)  //Verificar se houve erros na conexão
-  {
-    die("Erro ao conectar ao banco de dados.");
-  }
-  /*// Verificar se existe uma edição aberta dentro do prazo
-  $data_atual = date('Y-m-d H:i:s');
-  $consulta = "SELECT COUNT(*) AS total FROM SUBMISSOES WHERE INICIO <= '$data_atual' AND FIM >= '$data_atual'";
-  $resultado = $conexao->querySingle($consulta);
-  if ($resultado > 0) echo "Você pode submeter relatórios.";
-  else echo "Não há nenhuma edição aberta dentro do prazo para submissão de relatórios.";
-  // Fechar conexão com o banco de dados
-  $conexao->close();*/
-?-->
+<?php
+  $conexao = new SQLite3('C:\Users\Acer\Documents\BD\bd_pesta.db');
+  if(!$conexao) die("Erro ao conectar ao banco de dados."); //Verificar se houve erros na conexão
+  date_default_timezone_set("Europe/Lisbon");
+  $datual = date('Y-m-d H:i:s');  //Data de hoje
+?>
 <html lang="pt">
   <head>
     <title>Submiss&atilde;o de relat&oacute;rios</title>
     <script>
-        function mostrar() {
-            /*var selecao = document.getElementById("FiltroDisciplinas");
-            var conteudo = document.getElementById("conteudo");
-
-            //Lógica para determinar o conteúdo a ser exibido com base na opção selecionada
-            if (selecao.value == "0") {
-                conteudo.innerHTML = "Você selecionou: Todas as Unidades Curriculares";
-            } else if (selecao.value == "1") {
-                conteudo.innerHTML = "Você selecionou: Laborat&oacute;rio de Sistemas (LABSI)";
-            } else if (selecao.value == "2") {
-                conteudo.innerHTML = "Você selecionou: Projeto / Est&aacute;gio (PESTA)";
-            }*/
-            var selecao = document.getElementById("FiltroDisciplinas").value;
-            // Ocultar todas as divs
-            document.getElementById("todos").style.display = "none";
-            document.getElementById("lab_si").style.display = "none";
-            document.getElementById("pesta").style.display = "none";
-            // Mostrar a div correspondente à opção selecionada
-            if (selecao === "0") {
-              document.getElementById("todos").style.display = "block";
-            } else if (selecao === "1") {
-              document.getElementById("lab_si").style.display = "block";
-            } else if (selecao === "2") {
-              document.getElementById("pesta").style.display = "block";
-            }
+        function mostrar() 
+        {
+          var uc = document.getElementById("FiltroDisciplinas").value;
+          document.getElementById("todos").style.display = "none";
+          document.getElementById("labsi").style.display = "none";
+          document.getElementById("pesta").style.display = "none";
+          if(uc == "0") document.getElementById("todos").style.display = "block";
+          else if(uc == "1") document.getElementById("labsi").style.display = "block";
+          else if(uc == "2") document.getElementById("pesta").style.display = "block";
         }
     </script>
   </head>
   <body>
     <?php
-      
       session_set_cookie_params(['httponly' => true]);  //Proteção contra roubos de sessão
       session_start();  //Inicio de sessão
 
@@ -68,11 +42,20 @@
         echo "</br>";
         //Selecionar UC
         echo "<br>Listar Unidades Curriculares:
-        <select name='FiltroDisciplinas' size='' style='width:100%;' onchange='mostrar()'>
-          <option value='0' selected>Todas as Unidades Curriculares</option>
+        <select id='FiltroDisciplinas' name='FiltroDisciplinas' size='' style='width:100%;' onchange='mostrar()'>
+          <option value='0' selected>Ver todas as Unidades Curriculares</option>
           <option value='1' >Laborat&oacute;rio de Sistemas (LABSI)</option>
           <option value='2' >Projeto / Est&aacute;gio (PESTA)</option>
         </select>";
+        echo "<br><div id='todos' style='display: none;'>
+          <!-- Campos de submissão para todas as Unidades Curriculares -->
+        </div>
+        <div id='labsi' style='display: none;'>
+          <!-- Campos de submissão para Laboratório de Sistemas (LABSI) -->
+        </div>
+        <div id='pesta' style='display: none;'>
+          <!-- Campos de submissão para Projeto / Estágio (PESTA) -->
+        </div>"
         //Exibir botões para criar, editar/adicionar ou excluir relatórios da UC selecionada
     ?>
     <form action="" enctype="multipart/form-data" method="POST">
@@ -92,50 +75,57 @@
           }
         }
       }
+//---------------------------------SEPARADOR PROFESSOR-ALUNO---------------------------------\\
       else
       {
-        echo "Bem-vindo, aluno nº" . $_SESSION['user_aka'] . "! "
+        echo "<b><h2>Bem-vindo, aluno nº" . $_SESSION['user_aka'] . "!"
       ?>
       <form action="login.php" style="display: inline; float: right;" method="POST">
           <input type="submit" name="logout" value="Logout"/>
       </form>
       <?php
-        echo "</br>";
-        //Exibir botões para visualizar UCs e respetivos campos de submissão (com prazos visíveis)
-        echo "<br>Listar Unidades Curriculares:
-        <select name='FiltroDisciplinas' size='' style='width:100%;' onchange='mostrar()'>
-          <option value='0' selected>Todas as Unidades Curriculares</option>
+        echo "</b></h2>";
+        //Botões para visualizar UCs e respetivos campos de submissão (com prazos visíveis)
+        echo "<br><b><h3>Listar Unidades Curriculares:</b>
+        <select id='FiltroDisciplinas' name='FiltroDisciplinas' size='' style='width:100%;' onchange='mostrar()'>
+          <option value='0' selected>Ver todas as Unidades Curriculares</option>
           <option value='1' >Laborat&oacute;rio de Sistemas (LABSI)</option>
           <option value='2' >Projeto / Est&aacute;gio (PESTA)</option>
-        </select>";
-        echo "<br><div id='todos' style='display: none;'>
-          <!-- Campos de submissão para todas as Unidades Curriculares -->
-        </div>
-        <div id='lab_si' style='display: none;'>
-          <!-- Campos de submissão para Laboratório de Sistemas (LABSI) -->
-        </div>
-        <div id='pesta' style='display: none;'>
-          <!-- Campos de submissão para Projeto / Estágio (PESTA) -->
-        </div>"
+        </select></h3>";
+        echo "<div id='todos' style='display: block; text-align:center;'><h3><b>Escolha, em cima, uma Unidade Curricular para ver os campos de submissão disponíveis.</b></div>";
+        echo "<div id='labsi' style='display: none;'><h3><b>Campos de submissão para Laboratório de Sistemas (LABSI):</b></h3>";
+          //Verificar se existe uma edição aberta dentro do prazo
+          $consulta = "SELECT COUNT(*) AS total FROM SUBMISSOES WHERE INICIO <= '$datual' AND FIM >= '$datual'";
+          $resultado = $conexao->querySingle($consulta);
+          if($resultado > 0) 
+          {
+            echo "Você pode submeter relatórios.";
     ?>
     <form action="" enctype="multipart/form-data" method="POST">
 				<input type="file" name="file"/>
         <input type="submit" name="enviar" value="Submeter"/>
     </form>
     <?php
-        if(isset($_POST["enviar"]))
-        {
-          $arq = $_FILES["file"];
-          $narq = explode(".", $arq["name"]);
-          if($narq[sizeof($narq)-1] != "pdf") die("Não é possível dar upload do arquivo");
-          else
-          {
-            move_uploaded_file($arq["tmp_name"], "relatorios/" . $arq["name"]);
-            echo "Upload realizado";
+            if(isset($_POST["enviar"]))
+            {
+              $arq = $_FILES["file"];
+              $narq = explode(".", $arq["name"]);
+              if($narq[sizeof($narq)-1] != "pdf") die("Não é possível dar upload do arquivo");
+              else
+              {
+                move_uploaded_file($arq["tmp_name"], "relatorios/" . $arq["name"]);
+                echo "Upload realizado";
+              }
+            }
           }
-        }
+          else echo "<br><b>Não há nenhuma edição aberta dentro do prazo para submissão de relatórios.";
+          echo "</div>";
+          echo "<div id='pesta' style='display: none;'><h3><b>Campos de submissão para Projeto / Estágio (PESTA):</b></h3>";
+          echo "</div>";
       }
     ?>
-    <div id="conteudo"></div> 
+    <?php
+      $conexao->close();  //Fechar conexão com o banco de dados
+    ?>
   </body>
 </html>
